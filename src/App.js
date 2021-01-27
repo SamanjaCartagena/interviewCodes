839316c6632607ae4f61a0269e6a79f4c0331f9b
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-01-27 17:37:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state={
+      message:"Welcome to React World"
+    }
+    
+    this.activate= this.activate.bind(this);
+    
+  }
+    activate() {
+      this.setState(state =>({
+        message:"Hi"
+      }));
+   }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+        <button onClick={this.activate}>Click</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/components/myComp.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+export default ()=>{
+    return (<div>Hi there I am now loaded!</div>);
+}
