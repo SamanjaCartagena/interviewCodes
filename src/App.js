fec28ee470784c68e71cb99ebb7414028041ae0a
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-01-28 00:09:57 -0500

    formhandling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{Component} from 'react';
-
-
+import React, { Component } from 'react'
+import './App.css'
+import Form from './components/Form'
 export class App extends Component {
-  
-  constructor(props){
-    super(props)
-    this.state={
-      message:"Welcome to React World!"
-    }
-    
-    this.activate= this.activate.bind(this);
-    
-  }
-    activate() {
-      this.setState(state =>({
-        message:"Hi"
-      }));
-   }
   render() {
     return (
-      <div>
-        <h1>{this.state.message}</h1>
-        <button onClick={this.activate}>Click</button>
+      
+      <div className="App">
+        <Form/>
       </div>
     )
   }
